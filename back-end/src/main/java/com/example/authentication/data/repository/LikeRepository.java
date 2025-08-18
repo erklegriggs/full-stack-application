1b4c5dfc2499506f7c6a0db3805854c78806d2c3
Eric Griggs <eric.lucas.griggs@gmail.com>
2025-08-18 10:09:19 -0500

    Issue #27 - Repositories completed.

--- back-end/src/main/java/com/example/authentication/data/repository/LikeRepository.java
+++ back-end/src/main/java/com/example/authentication/data/repository/LikeRepository.java
@@ -1,4 +1,29 @@
 package com.example.authentication.data.repository;
 
-public interface LikeRepository {
+import com.example.authentication.data.model.Like;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface LikeRepository extends JpaRepository<Like, Integer> {
+
+    @Query(value = "SELECT * FROM likes WHERE user_id = :userId ORDER BY created_at DESC", nativeQuery = true)
+    List<Like> findLikesByUserId(String userId);
+
+    @Query(value = "SELECT COUNT(*) > 0 FROM likes WHERE user_id = :userId AND mixtape_id = :mixtapeId", nativeQuery = true)
+    boolean likedByUserIdMixtapeId(String userId, int mixtapeId);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM likes WHERE user_id = :userId AND mixtape_id = :mixtapeId", nativeQuery = true)
+    void removeByUserIdMixtapeId(String userId, int mixtapeId);
+
+
+
+
+
+
 }
--- back-end/src/main/java/com/example/authentication/data/repository/MixtapeRepository.java
+++ back-end/src/main/java/com/example/authentication/data/repository/MixtapeRepository.java
@@ -8,10 +8,8 @@ import java.util.List;
 
 public interface MixtapeRepository extends JpaRepository<Mixtape, Integer> {
 
+
     @Query(value = "SELECT mixtape_id, user_id, genre_id, name, description, date, mixtape_pic_url FROM mixtapes WHERE user_id = :userId ORDER BY date DESC", nativeQuery = true)
     List<Mixtape> findByUserId(String userId);
-
-    @Query(value = "SELECT mixtape_id, user_id, genre_id, name, description, date, mixtape_pic_url FROM mixtapes ORDER BY date DESC", nativeQuery = true)
-    List<Mixtape> findAll();
 
 }
