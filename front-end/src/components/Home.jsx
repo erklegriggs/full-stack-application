2d0a88bcc8d0c0abc35f1badda123735745ba0ec
Eric Griggs <eric.lucas.griggs@gmail.com>
2025-08-19 16:32:04 -0500

    Issue #28 - Home page designed.

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -15,27 +15,40 @@ export default function Home() {
 
 
     return (
-        <div className="homeContainer">
-            <div className="homeContent">
-                <h1>MxTape</h1>
-                <p>Stream. Share. Discover.</p>
-                <p>Discover.</p>
-                <img src="/images/MxTapeLogo.jpg" alt="MxTape Company Logo"/>
-                <p>Log in to share your sound now!</p>
+        <>
+            <div className="topBanner">Where Music Meets Community.</div>
+            <div className="homeContainer">
+                <div className="homeContent">
+                    <h1>M<span className="letterX">x</span>Tape</h1>
+                    <p>Stream. Share.</p>
+                    <p>Discover.</p>
+                    <img src="/images/MxTapeLogo.jpg" alt="MxTape Company Logo"/>
+                    <p>Log in to share your sound now!</p>
 
-                <div className="navigation">
-                    <Link to="/login" className="loginButton">login</Link>
-                    <br/>
-                    <Link to="/signup" className="signupButton">sign up</Link>
-                    <br/>
-                    <Link to="/secured" className="securedButton">secured</Link>
-                    <br/>
-                    {localStorage.getItem('key') && (
-                    <button onClick={() => {localStorage.clear(); window.location.reload();}}>Logout</button>
-                    )}
+                    <div className="navigation">
+                        <Link to="/login" className="loginButton">login</Link>
+                        <br/>
+                        <Link to="/signup" className="signupButton">sign up</Link>
+                        <br/>
+                        <Link to="/secured" className="securedButton">secured</Link>
+                        <br/>
+                        {localStorage.getItem('key') && (
+                        <button onClick={() => {localStorage.clear(); window.location.reload();}}>Logout</button>
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <footer className="footer">
+                <p>Promoting your favorite music since 2025.</p>
+                <div className="copyright">
+                    <p>© MxTape Inc, 2025</p>
+                    <p>1011 Musically Drive,</p>
+                    <p>San Francisco, CA</p>
+                </div>
+            </footer>
+
+        </>
 
     )
 }
