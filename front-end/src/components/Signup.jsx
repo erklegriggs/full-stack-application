1a10de32e055f0554e81606b94b2c83ae454e419
Eric Griggs <eric.lucas.griggs@gmail.com>
2025-08-19 21:34:57 -0500

    Committing the rest of incomplete work.

--- front-end/src/components/Signup.jsx
+++ front-end/src/components/Signup.jsx
@@ -3,13 +3,28 @@ import {signup} from "../utilities/apiUtilities";
 
 import { jwtDecode } from 'jwt-decode'
 import {useNavigate} from "react-router-dom";
+import FileUpload from "./FileUploader.jsx";
 
 export default function Signup() {
     const navigate = useNavigate();
     const [Username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [profilePicFile, setProfilePicFile] = useState(null);
+    const [profilePicPreview, setProfilePicPreview] = useState(null);
     let [signupErrors, setSignupErrors] = useState([]);
+
+    const handleProfilePicSubmit = (e) => {
+        const file = e.target.files[0];
+        setProfilePicFile(file);
+        if(file) {
+            const reader = new FileReader();
+            reader.onload = (e) => setProfilePicFile(e.target.result);
+            reader.readAsDataURL(file);
+        } else {
+            setProfilePicPreview(null);
+        }
+    }
 
     const validateForm = () => {
         let errors = [];
@@ -37,12 +52,26 @@ export default function Signup() {
 
         const payload = { username: Username, password: password };
 
-        signup(payload).then((response) => {
+        signup(payload).then(async (response) => {
             console.log(response)
             const claims = jwtDecode(response.token);
             localStorage.setItem('key', response.token);
             localStorage.setItem('role', claims.role[0].authority);
             localStorage.setItem('username', claims.sub);
+            if(profilePicFile) {
+                const formData = new FormData();
+                formData.append('profilePic', profilePicFile);
+                try {
+                    await fetch(`http://localhost:8080/api/users/${claims.userId}/profile-pic`, {
+                        method: "POST",
+                        headers: {'Authorization': `Bearer ${response.token}`},
+                        body: formData,
+                    })
+                } catch (error) {
+                    console.log("Profile picture upload failed! Error: " + error);
+                }
+            }
+
             navigate('/mixtapes');
         }, []);
     }
@@ -76,6 +105,24 @@ export default function Signup() {
                     />
 
                 <br/>
+
+                <label>Avatar</label>
+                <div className={`avatarPreview ${profilePicPreview ? 'hasImage' : ''}`}>
+                    {profilePicPreview ? (
+                        <img src={profilePicPreview} alt="Avatar Preview" />
+                    ):(
+                        <div className="avatarPreviewPlaceholder">
+                        Choose Image
+                        </div>
+                    )}
+                </div>
+
+                <input
+                    type="file"
+                    accept=".jpg, .png, .jpeg"
+                    onChange={handleProfilePicSubmit}
+                />
+
                 <button type="submit">Sign Up</button>
             </form>
             <h3>Signup Response:</h3>
