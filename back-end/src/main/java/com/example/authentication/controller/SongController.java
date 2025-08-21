606abd74890e71711e7e761e85dea4d10fc71fc9
Eric Griggs <eric.lucas.griggs@gmail.com>
2025-08-21 16:44:23 -0500

    Committing updated Song controller & repository

--- back-end/src/main/java/com/example/authentication/controller/SongController.java
+++ back-end/src/main/java/com/example/authentication/controller/SongController.java
@@ -1,6 +1,8 @@
 package com.example.authentication.controller;
 
+import com.example.authentication.data.model.Mixtape;
 import com.example.authentication.data.model.Song;
+import com.example.authentication.data.repository.MixtapeRepository;
 import com.example.authentication.data.repository.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,13 +11,25 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/songs")
 public class SongController {
 
     @Autowired
     SongRepository songRepository;
+
+    @Autowired
+    MixtapeRepository mixtapeRepository;
+
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<Song>> getAllSongs() {
+        List<Song> songs = songRepository.findAll();
+        return ResponseEntity.ok(songs);
+    }
 
     @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @GetMapping("/mixtape/{mixtapeId}")
@@ -25,9 +39,25 @@ public class SongController {
     }
 
     @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
-    @PostMapping
-    public ResponseEntity<Song> addSong(@RequestBody Song song) {
-        song = songRepository.save(song);
+    @PostMapping("/mixtape/{mixtapeId}")
+    public ResponseEntity<Song> addSongToMixtape(@PathVariable Integer mixtapeId, @RequestBody Song song) {
+        Optional<Mixtape> mixtapeOptional = mixtapeRepository.findById(mixtapeId);
+        if(mixtapeOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        song.setMixtape(mixtapeOptional.get());
+        song = songRepository.saveAndFlush(song);
         return ResponseEntity.status(HttpStatus.CREATED).body(song);
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @DeleteMapping("/{songId}")
+    public ResponseEntity<Void> deleteSong(@PathVariable Integer songId) {
+        if(songRepository.existsById(songId)) {
+            songRepository.deleteById(songId);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
--- back-end/src/main/java/com/example/authentication/data/repository/SongRepository.java
+++ back-end/src/main/java/com/example/authentication/data/repository/SongRepository.java
@@ -2,9 +2,13 @@ package com.example.authentication.data.repository;
 
 import com.example.authentication.data.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface SongRepository extends JpaRepository<Song, Integer> {
-    List<Song> findByMixtapeId(Integer mixtapeId);
+
+    @Query(value = "SELECT * from songs WHERE mixtape_id = :mixtape_id", nativeQuery = true)
+    List<Song> findByMixtapeId(@Param("mixtape_id") int mixtapeId);
 }
